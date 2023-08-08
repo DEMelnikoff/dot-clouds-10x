0c8fe12e6c7a7cd243e722d0b791106b8c268e78
David E Melnikoff <davidemelnikoff@gmail.com>
2023-08-08 14:16:37 -0700

    Update global-funcs.js

--- jsFiles/global-funcs.js
+++ jsFiles/global-funcs.js
@@ -27,7 +27,7 @@ const filename = `${subject_id}.csv`;
 const save_data = {
     type: jsPsychPipe,
     action: "save",
-    experiment_id: "NmRYj5s6woqK",
+    experiment_id: "EcXUw8IlQEss",
     filename: filename,
     data_string: ()=>jsPsych.data.get().csv()
 };
